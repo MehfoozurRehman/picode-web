7cc32f76f20a406a2b18e07e1b7260df2a52220c
Arslan Iftikhar <a44222666@gmail.com>
2022-04-03 16:03:20 +0500

    Done
    without responsiveness😁

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,259 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <h1>App</h1>
-  )
+    <>
+      <div id="home" className="home__jumbotron__wrapper">
+        <div className="home__navigation__wrapper">
+          <div className="home__navigation__logo">
+            <img src="src\img\logo.png" alt="logo" />
+          </div>
+          <nav className="home__navigation__navbar">
+            <ul>
+              <li>
+                <a href="#">SignUp</a>
+              </li>
+              <li>
+                <a href="#">Login</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="home__jumbotron__bg__wrapper">
+          <img
+            className="home__jumbotron__bg__img"
+            src="src\img\jumbotron__bg.png"
+            alt="bg image"
+          />
+        </div>
+        <div className="home__jumbotron__content">
+          <div className="home__jumbotron__content__left">
+            <h1 className="home__jumbotron__content__left__heading">
+              Introducing Picode Landing Page
+            </h1>
+            <p className="home__jumbotron__content__left__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              nesciunt id nulla iste ab ea repudiandae tenetur accusamus
+              possimus in.
+            </p>
+            <a href="#" className="home__jumbotron__content__left__button">
+              Join as Partner
+            </a>
+          </div>
+          <div className="home__jumbotron__content__right">
+            <img src="src\img\jumbotron__mobile.png" alt="mobile" />
+          </div>
+        </div>
+      </div>
+      <section className="cards__container">
+        <div className="card">
+          <div className="card__img">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              className="card__img__svg"
+            >
+              <g id="clock" transform="translate(-0.5 -0.5)">
+                <circle
+                  id="Ellipse_24"
+                  data-name="Ellipse 24"
+                  cx="15.5"
+                  cy="15.5"
+                  r="15.5"
+                  transform="translate(2 2)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+                <path
+                  id="Path_889"
+                  data-name="Path 889"
+                  d="M12,6v9.346l6.231,3.115"
+                  transform="translate(5.577 2.231)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="card__heading">Save your Time</div>
+          <div className="card__para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, id.
+          </div>
+          <div className="card__link">
+            <a href="#">More &#8594;</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__img">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              className="card__img__svg"
+            >
+              <g id="clock" transform="translate(-0.5 -0.5)">
+                <circle
+                  id="Ellipse_24"
+                  data-name="Ellipse 24"
+                  cx="15.5"
+                  cy="15.5"
+                  r="15.5"
+                  transform="translate(2 2)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+                <path
+                  id="Path_889"
+                  data-name="Path 889"
+                  d="M12,6v9.346l6.231,3.115"
+                  transform="translate(5.577 2.231)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="card__heading">Save your Time</div>
+          <div className="card__para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, id.
+          </div>
+          <div className="card__link">
+            <a href="#">More &#8594;</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__img">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              className="card__img__svg"
+            >
+              <g id="clock" transform="translate(-0.5 -0.5)">
+                <circle
+                  id="Ellipse_24"
+                  data-name="Ellipse 24"
+                  cx="15.5"
+                  cy="15.5"
+                  r="15.5"
+                  transform="translate(2 2)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+                <path
+                  id="Path_889"
+                  data-name="Path 889"
+                  d="M12,6v9.346l6.231,3.115"
+                  transform="translate(5.577 2.231)"
+                  fill="none"
+                  stroke="#0e0e17"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="3"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="card__heading">Save your Time</div>
+          <div className="card__para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, id.
+          </div>
+          <div className="card__link">
+            <a href="#">More &#8594;</a>
+          </div>
+        </div>
+      </section>
+      <section id="app" className="text__section">
+        <div className="text__section__wrapper">
+          <img src="src\img\text__section__bg.png" alt="bg__text__section" />
+          <div className="text__section__content">
+            <h2 className="text__section__content__heading">
+              Introducding Picode Landing Page
+            </h2>
+            <div className="text__section__content__para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+              illo quasi quas quod maiores quis!
+            </div>
+            <a href="" className="text__section__content__button">
+              Download App
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="chat" className="chat__section">
+        <div className="chat__section__container">
+          <div className="chat__section__content">
+            <h2 className="chat__section__heading">Start chat on WhatsApp</h2>
+            <div className="chat__section__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              magnam sunt assumenda quam, pariatur ipsum!
+            </div>
+            <a href="#" className="chat__section__button">
+              Chat Now
+            </a>
+          </div>
+        </div>
+      </section>
+      <footer className="footer__section">
+        <div className="footer__section__bg">
+          <img src="src\img\footer.png" alt="footer" />
+        </div>
+        <div className="footer__section__content">
+          <div className="footer__section__newsletter">
+            <h2 className="footer__section__heading">
+              Don't miss out. Stay update
+            </h2>
+            <form action="" className="footer__section__form">
+              <input type="email" placeholder="Enter your email" className="footer__section__email"/>
+              <input type="submit" placeholder="Subscribe"
+              className="footer__section__button" />
+            </form>
+          </div>
+          <nav className="footer__section__menu">
+            <ul className="footer__section__list">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#app">App</a>
+              </li>
+              <li>
+                <a href="#chat">Chat</a>
+              </li>
+              <li>
+                <a href="#home">Partner</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer__section__copyright">
+            &copy; All copyright are reserved.
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="app__container">
     <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
